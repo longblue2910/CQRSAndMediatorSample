f2966e9c55922fff49607e2b261bd5ceb134ab29
longblue2910 <tienlong291099@gmail.com>
2021-07-07 16:52:28 +0700

    Add project files.

---
+++ CQRSAndMediator/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSAndMediator.Entities
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public string OrderName { get; set; }
+        public DateTime DateOrder { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public double Amount { get; set; }
+        public Guid OrderPersonId { get; set; }
+    }
+}
